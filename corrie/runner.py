edbd5da1bc90ebb7af7ec9cfdf089ab70c09fecd
JasonGlazer <jglazer@gard.com>
2019-10-17 09:18:09 -0500

    Add path to allow running from command line.

--- corrie/runner.py
+++ corrie/runner.py
@@ -1,6 +1,9 @@
 import gettext
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from corrie.interface.application import CorrieApplication
+
 
 gettext.install("app")  # replace with the appropriate catalog name
 app = CorrieApplication(0)
